ce4acff02de415ab41f238a948a2c50a70c58320
Bartosz Gracjan Dorobek <b.dorobek@stud.elka.pw.edu.pl>
2023-12-06 22:19:38 +0000

    updates

--- Job2/Mapper.py
+++ Job2/Mapper.py
@@ -8,13 +8,10 @@ import csv
 cascade_filename = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(cascade_filename)
 
-
 def detect_face(image, scaleFactor, minNeighbors, minSize):
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(image_gray, scaleFactor=scaleFactor, minNeighbors=minNeighbors,
-                                          minSize=minSize)
+    faces = face_cascade.detectMultiScale(image_gray, scaleFactor=scaleFactor, minNeighbors=minNeighbors, minSize=minSize)
     return faces
-
 
 def process_image(line):
     # Parse the CSV line
@@ -25,7 +22,6 @@ def process_image(line):
 
     # Return a tuple with image name and face coordinates
     return image_name, (x, y, width, height)
-
 
 def main():
     scaleFactor = 1.2
@@ -45,5 +41,5 @@ def main():
             for face in faces:
                 print(f"{image_name},{','.join(map(str, face))}")
 
-                if __name__ == "__main__":
-                    main()
+if __name__ == "__main__":
+    main()
--- Job2/run.sh
+++ Job2/run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Set your Hadoop home directory
-HADOOP_HOME=/path/to/your/hadoop
+#HADOOP_HOME=/path/to/your/hadoop
+HADOOP_BIN=${HADOOP_HOME}/bin/hadoop
 
 # Input and output directories
-INPUT_DIR=input_directory
-OUTPUT_DIR=output_directory
+INPUT_PATH="/home/usermr/examples/input/images_small/"
+OUTPUT_PATH="/home/usermr/examples/output/face_count/"
 
 # Upload input data to HDFS
 hadoop fs -put $INPUT_DIR/*.csv input
 
 # Run Hadoop Streaming job
-hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
+hadoop jar $HADOOP_BIN/share/hadoop/tools/lib/hadoop-streaming-*.jar \
     -mapper Mapper.py \
     -reducer Reducer.py \
     -input input \
