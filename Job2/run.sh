8fdd3cd195f2c7fef01bf1cb7fe34acec90969e2
Bartosz Gracjan Dorobek <b.dorobek@stud.elka.pw.edu.pl>
2023-12-06 22:03:36 +0000

    update base run.sh

--- Job2/run.sh
+++ Job2/run.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+# Set your Hadoop home directory
+HADOOP_HOME=/path/to/your/hadoop
+
+# Input and output directories
+INPUT_DIR=input_directory
+OUTPUT_DIR=output_directory
+
+# Upload input data to HDFS
+hadoop fs -put $INPUT_DIR/*.csv input
+
+# Run Hadoop Streaming job
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar \
     -mapper Mapper.py \
     -reducer Reducer.py \
-    -input input_directory/*.csv \
-    -output output_directory
+    -input input \
+    -output $OUTPUT_DIR
+
+# Display the output
+hadoop fs -cat $OUTPUT_DIR/*
