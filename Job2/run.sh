8c165c33b85b8d26d5b94a461853533f8e6e4a8c
Bartosz Gracjan Dorobek <b.dorobek@stud.elka.pw.edu.pl>
2023-12-11 14:23:44 +0000

    modification in run, delete MainApp

--- Job2/run.sh
+++ Job2/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Set your Hadoop home directory
-#HADOOP_HOME=/path/to/your/hadoop
+#HADOOP_HOME=/work/hadoop/hadoop
+HADOOP_HOME=/work/hadoop/hadoop-3.3.1
 HADOOP_BIN=${HADOOP_HOME}/bin/hadoop
 
 # Input and output directories
@@ -9,14 +10,14 @@ INPUT_PATH="/home/usermr/examples/input/images_small/"
 OUTPUT_PATH="/home/usermr/examples/output/face_count/"
 
 # Upload input data to HDFS
-hadoop fs -put $INPUT_DIR/*.csv input
+hadoop fs -put $INPUT_PATH/*.csv input
 
 # Run Hadoop Streaming job
-hadoop jar $HADOOP_BIN/share/hadoop/tools/lib/hadoop-streaming-*.jar \
+hadoop jar ${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
     -mapper Mapper.py \
     -reducer Reducer.py \
     -input input \
-    -output $OUTPUT_DIR
+    -output $OUTPUT_PATH
 
 # Display the output
-hadoop fs -cat $OUTPUT_DIR/*
+$HADOOP_BIN fs -cat $OUTPUT_PATH/*
